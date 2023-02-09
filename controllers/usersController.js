49307e4c9c3ac717a356339a00278939619ee302
Ryan Mazri <ryan.mazri@yahoo.fr>
2023-02-09 13:44:01 +0100

    v0.1.0: added authentification

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,134 +1,122 @@
 const connection = require('../src/database');
 const jwt = require ('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 
 const selectAllUsers = () => {
-return new Promise((resolve, reject) => {
-connection.query('SELECT * FROM users', function(err, results) {
-if (err) {
-reject(err);
-} else {
-resolve(results);
-}
-});
-});
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM users', function(err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
 };
 const selectUser = (id) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     connection.query(`SELECT * FROM users WHERE id = ${id}`, function(err, results) {
-    if (err) {
-    reject(err);
-    } else {
-    resolve(results);
-    }
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
     });
-    });
-    };
+  });
+};
     
 const addUser = (userData) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (!userData.username || !userData.email || !userData.password) {
-    reject(new Error("username, email and password are required"));
+      reject(new Error("username, email and password are required"));
     }
     const allowedData = {
-    username: userData.username,
-    email: userData.email,
-    password: userData.password
+      username: userData.username,
+      email: userData.email,
+      password: userData.password
     };
     connection.query('INSERT INTO users SET ?', allowedData, function(err, results) {
-    if (err) {
-    reject(err);
-    } else {
-    resolve(results);
-    }
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
     });
+  });
+};
+
+const deleteUser = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(`DELETE FROM users WHERE id = ${id}`, function(err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
     });
-    };
+  });
+};
       
+const updateUser = (id, userData) => {
+  return new Promise((resolve, reject) => {
+    connection.query(`UPDATE users SET ? WHERE id = ${id}`, userData, function(err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
 
+const comparePassword = (password1, password2) =>{
+  if (password1 === password2){
+    return true
+  } else {
+    return false
+  }
+}
 
-      const deleteUser = (id) => {
-        return new Promise((resolve, reject) => {
-          connection.query(`DELETE FROM users WHERE id = ${id}`, function(err, results) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(results);
-            }
-          });
-        });
-      };
-      
-      const updateUser = (id, userData) => {
-        return new Promise((resolve, reject) => {
-          connection.query(`UPDATE users SET ? WHERE id = ${id}`, userData, function(err, results) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(results);
-            }
-          });
-        });
-      };
-
-const generateJWTToken = (username) => {
-    const secret = process.env.JWT_SECRET_KEY;
-    const expiresIn = "1d";
-    const token = jwt.sign({ username }, secret, { expiresIn });
-    return token
+const generateJWTToken = (user) => {
+  return jwt.sign(user, process.env.JWT_SECRET_KEY, {expiresIn: "1800s"})
 }
 
 const checkUser = async (userData) => {
+  try {  
     if (!userData.username || !userData.password) {  
-    throw new Error("Missing username or password")
+      throw new Error("Missing username or password")
     }
-    const checkUsername = new Promise((resolve, reject) => {
-        connection.query(`SELECT username FROM users WHERE username = '${userData.username}'`, function (err, results){
-            if (err) {
-                reject(err)
-            } else {
-                resolve(results)
-            }})
-        })
-const checkPassword = new Promise((resolve, reject) => {
-    connection.query(`SELECT password FROM users where username = '${userData.username}'`, function (err, results){
+    const checkUsername = await new Promise((resolve, reject) => {
+      connection.query(`SELECT username FROM users WHERE username = '${userData.username}' LIMIT 1`, function (err, results){
         if (err) {
-            reject(err)
+          reject(err)
         } else {
-            resolve(results)
+          resolve(results)
         }})
-    })
-const comparePassword = (password1, password2) =>{
-    if (password1 === password2){
-        return true
-    } else {
-        return false
+      })
+    if (!checkUsername){
+      throw new Error("Username not found")
     }
-}
-
-    return async function authentification(){
-        try {
-            const usernameResult = await checkUsername;
-            if (!usernameResult){
-                throw new Error("Username not found")
-            }
-            const passwordResult = await checkPassword;
-            if (!passwordResult){
-                throw new Error("Password not found")
-            }
-            const isPasswordMatch = comparePassword(userData.password, passwordResult[0].password);
-            if (!isPasswordMatch) {
-                throw new Error("Password is incorrect")
-            }  
-                return generateJWTToken(userData.username);
-        } catch (err) {
-            console.error(err);
-            throw err;
+    const checkPassword = await new Promise((resolve, reject) => {
+      connection.query(`SELECT password FROM users where username = '${userData.username}' LIMIT 1`, function (err, results){
+        if (err) {
+          reject(err)
+        } else {
+          resolve(results)
         }
-    }
+      })
+    })
+    const passwordMatching = comparePassword(userData.password, checkPassword[0].password)
+      if(passwordMatching == false){
+        throw new Error("Password incorrect")
+      }
+    return generateJWTToken(userData)
+  } catch (err) {
+    console.error(err)
+    throw error
+  }
 }
-
- 
 
     module.exports = {
   selectAllUsers,
@@ -136,5 +124,5 @@ const comparePassword = (password1, password2) =>{
   addUser,
   deleteUser,
   updateUser,
-  checkUser
+ checkUser
 };
